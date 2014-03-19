008a3fade966ddacf509438880c73997bf3a7658
Mark Evans <ga1j1n@mac.com>
2014-03-19 15:01:53 -0600

    Can now match a given date and return the expected name.

--- birthday-kata/lib/birthday_kata.rb
+++ birthday-kata/lib/birthday_kata.rb
@@ -1,8 +1,22 @@
+require 'csv'
+
 class BirthdayKata
   def initialize (csv_file)
     @csv_file = csv_file
+    @birthday_array = []
+    CSV.foreach(csv_file, {headers: true}) do |row|
+      @birthday_array << row
+    end
   end
   def get_names(date)
-    "Gehard, Mike"
+    #@birthday_array[2]["last_name"] + "," + @birthday_array[2][" first_name"]
+    @names_array = []
+    @birthday_array.each do |row|
+      if date == row[" date_of_birth"].strip
+        @names_array << row["last_name"] + "," + row[" first_name"]
+      end
+    end
+    @names_array.join
   end
+
 end
--- birthday-kata/spec/birthday_spec.rb
+++ birthday-kata/spec/birthday_spec.rb
@@ -3,8 +3,14 @@ require 'birthday_kata'
 describe do
   it "returns a person whose birthday matches the sent date" do
     birthday_prog = BirthdayKata.new("employee_information.csv")
-    expected = birthday_prog.get_names(1987/05/02)
-    actual = "Gehard, Mike"
+    expected = "Gehard, Mike"
+    actual = birthday_prog.get_names("1987/05/02")
+    expect(actual).to eq expected
+  end
+  it "returns a person whose birthday matches the sent date" do
+    birthday_prog = BirthdayKata.new("employee_information.csv")
+    expected = "Durham, Kinsey"
+    actual = birthday_prog.get_names("1945/04/01")
     expect(actual).to eq expected
   end
 end
