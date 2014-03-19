2f59396b1cb1531f22930b06cd35c61640991150
Mark Evans <ga1j1n@mac.com>
2014-03-19 15:19:54 -0600

    Made a person class to hold new person information.

--- birthday-kata/lib/birthday_kata.rb
+++ birthday-kata/lib/birthday_kata.rb
@@ -6,6 +6,7 @@ class BirthdayKata
     @birthday_array = []
     CSV.foreach(csv_file, {headers: true}) do |row|
       @birthday_array << row
+      #@birthday_array << Person.new
     end
   end
   def get_names(date)
@@ -18,5 +19,15 @@ class BirthdayKata
     end
     @names_array.join
   end
+end
 
+
+class Person
+  attr_accessor :first_name, :last_name, :date_of_birth, :email
+  def initialize(fn, ln, dob, email)
+    @first_name = fn
+    @last_name = ln
+    @date_of_birth = dob
+    @email = email
+  end
 end
--- birthday-kata/spec/birthday_spec.rb
+++ birthday-kata/spec/birthday_spec.rb
@@ -1,6 +1,6 @@
 require 'birthday_kata'
 
-describe do
+describe BirthdayKata do
   it "returns a person whose birthday matches the sent date" do
     birthday_prog = BirthdayKata.new("employee_information.csv")
     expected = "Gehard, Mike"
@@ -11,6 +11,18 @@ describe do
     birthday_prog = BirthdayKata.new("employee_information.csv")
     expected = "Durham, Kinsey"
     actual = birthday_prog.get_names("1945/04/01")
+    expect(actual).to eq expected
+  end
+end
+
+describe Person do
+  it "takes information parameters at instantiation" do
+
+    cory = Person.new("Cory", "Leistikow", "1974/12/01", "cory.leistikow@gmail.com")
+
+    expected = "1974/12/01"
+    actual = cory.date_of_birth
+
     expect(actual).to eq expected
   end
 end
